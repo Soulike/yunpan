d1e75ec884f28b3556ba9cf993c5a07a41b3ff93
Soulike <316413310@qq.com>
2018-05-22 18:33:29 +0800

    修正端口

--- static/scripts/config.js
+++ static/scripts/config.js
@@ -2,7 +2,7 @@ const REGEXP = {
     EMAIL: /^[0-9A-z]+@([0-9A-z]+\.)+[0-9A-z]+$/
 };
 
-const [DOMAIN, PORT] = ['pan.soulike.tech', 8002];
+const [DOMAIN, PORT] = ['pan.soulike.tech', 80];
 
 const MSG = {
     ERROR: '网络异常，请重试'
