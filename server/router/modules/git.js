5a393a0ba2a4a0c6f920acb08fbed2d59515207b
Soulike <316413310@qq.com>
2018-05-26 10:20:09 +0800

    服务器自动更新增加 npm install

--- server/router/modules/git.js
+++ server/router/modules/git.js
@@ -16,12 +16,20 @@ module.exports = (router) =>
             asyncFunctions.gitUpdateAsync(config.PATH.SERVER_FILES_PATH)
                 .then(async (std) =>
                 {
-                    const {stdout,stderr} = std;
+                    const {stdout, stderr} = std;
                     log(`Server files update detected.\n${stdout}`);
                     if (stderr)
                     {
                         log(`Error when updating through git.\n${stderr}`);
                     }
+
+                    const {npmStdout, npmStderr} = await asyncFunctions.execAsync('npm install', {cwd: config.PATH.SERVER_FILES_PATH});
+                    log(`Server modules installing.\n${npmStdout}`);
+                    if (npmStderr)
+                    {
+                        log(`Error when installing server modules.\n${stderr}`);
+                    }
+
                     log(`Restarting server daemon.`);
                     const {stderr: execStderr} = await asyncFunctions.execAsync(`supervisorctl restart ${config.NAME.SUPERVISOR}`);
                     if (execStderr)
